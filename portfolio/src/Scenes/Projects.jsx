d5b968764db0abaa46f095f9b623861d60869f0b
christianaojo <christianaojoofficial.com>
2023-03-24 12:39:41 +0100

    third commit

--- portfolio/src/Scenes/Projects.jsx
+++ portfolio/src/Scenes/Projects.jsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 
 const Projects = () => {
   return (
-    <section id="projects" className="pt-20 pb-24">
+    <section id="projects" className="pt-40 pb-24">
       {/* HEADINGS */}
       <motion.div
         className="md:w-2/5 mx-auto text-center"
